c496e4a3e60babd797632a63c869853bc9ba59ed
Vlad Filipovich <fvfordota2@gmail.com>
2022-12-02 06:53:50 +0300

    + breadth_first_search.py

---
+++ breadth_first_search.py
@@ -0,0 +1,38 @@
+from collections import deque
+
+
+def person_is_seller(person):
+    return 'man' in person
+
+
+def mango_seller_search(graph):
+    search_queue = deque()
+    search_queue += graph[list(graph.keys())[0]]
+    verified = []
+
+    while search_queue:
+        person = search_queue.popleft()
+
+        if person not in verified:
+
+            if person_is_seller(person):
+                return f'{person} is a mango seller!'
+
+            else:
+                verified.append(person)
+                search_queue += graph[person]
+
+    return False
+
+
+graph = dict()
+graph['you'] = ['alice', 'bob', 'claire']
+graph['alice'] = ['peggy']
+graph['bob'] = ['anuj', 'peggy']
+graph['claire'] = ['thom', 'jonny']
+graph['anuj'] = []
+graph['peggy'] = []
+graph['thom'] = []
+graph['jonny'] = []
+
+print(mango_seller_search(graph))
